f458fb848ee6be42a326c25e2629995e0b171c45
zhuyujin <zyj940628@gmail.com>
2016-03-02 19:26:34 +0800

    10

--- src/car_move/basic_move.cpp
+++ src/car_move/basic_move.cpp
@@ -7,40 +7,26 @@
 void moveCallback(const std_msgs::String::ConstPtr& msg){
 	ROS_INFO("I heard: [%s]", msg->data.c_str());
 
-	// RaspiRobot::getInstance()->forwardBySpeed(50);
+	RaspiRobot::getInstance()->forwardBySpeed(50);
 	
-	// delay(5000);
+	delay(5000);
 
-	// RaspiRobot::getInstance()->forwardBySpeed(150);
+	RaspiRobot::getInstance()->forwardBySpeed(150);
 
-	// delay(5000);
-
-	digitalWrite(7, 0);
-	digitalWrite(0, 1);
-	digitalWrite(2, 0);
-	digitalWrite(3, 1);
+	delay(5000);
 }
 
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "basic_move");
 
-	wiringPiSetup();
-
-	pinMode(7, OUTPUT);
-	pinMode(0, OUTPUT);
-	pinMode(2, OUTPUT);
-	pinMode(3, OUTPUT);
+	RaspiRobot::init();
 
 	ros::NodeHandle n;
 
 	ros::Subscriber sub = n.subscribe("move_chatter", 1000, moveCallback);
 
 	ros::spin();
-
-	// RaspiRobot::init();
-
-	
 
 	return 0;
 }

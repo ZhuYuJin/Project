6690a4f2e92c3a4fcc0b05b8cc39ff60c9d5df5a
zhuyujin <zyj940628@gmail.com>
2016-04-19 15:31:05 +0800

    new1

---
+++ src/car_move/readVoltage.cpp
@@ -0,0 +1,20 @@
+// #include "RobotController.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void moveCallback(const std_msgs::String::ConstPtr& msg){
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "basic_move");
+
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("voltage_reader", 1000, moveCallback);
+
+	ros::spin();
+
+	return 0;
+}

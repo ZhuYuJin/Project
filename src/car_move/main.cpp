4c7a278af04b6866d88f0d3e66bc4d2450dc2ef3
zhuyujin <zyj940628@gmail.com>
2016-04-30 17:22:21 +0800

    new14

--- src/car_move/main.cpp
+++ src/car_move/main.cpp
@@ -38,7 +38,7 @@ int getRegionFromCam(){
 	delay(10000);
 
 	float degree = 0.0;
-	float unit_degree = 9.0;
+	float unit_degree = 15.0;
 	while(!barcode_exist && degree < 360.0){
 		degree += unit_degree/3; //data amendment
 		RaspiRobot::getInstance()->rotate_clockwise(unit_degree);
